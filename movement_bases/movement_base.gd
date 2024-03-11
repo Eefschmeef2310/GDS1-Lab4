aec1e4bb4d3f45656435e8d63761a398da2afb87
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-03-11 23:49:54 +1100

    Revert "Merge branch 'dev' into player-animation"
    This reverts commit 64266f927e71a0bd7e99715dea94f4e4c0ef956f, reversing
    changes made to e8eddfe510c37c94671d29e2786bdbbcf9a2729a.

--- movement_bases/movement_base.gd
+++ movement_bases/movement_base.gd
@@ -3,21 +3,34 @@ class_name MovementScript
 #Authored by Ethan. Please consult for any modifications or major feature requests.
 
 #region Variables
-	#Exported Variables
+#Signals
+
+#Enums
+
+#Constants
+
+#Exported Variables
+@export var target: CharacterBody2D
+
 @export_group("Parameters")
 @export var speed: float = 100
 
+#Onready Variables
+
 #Other Variables (please try to separate and organise!)
 var direction:Vector2
+
 #endregion
 
 #region Godot methods
-func _physics_process(_delta):
-	owner.velocity = direction * speed
-	owner.move_and_slide()
-#endregion
+func _ready():
+	#Runs when all children have entered the tree
+	pass
 
-#region Other methods
-func set_direction(dir: Vector2):
-	direction = dir
-#end region
+func _process(delta):
+	direction = Input.get_vector("Left", "Right", "Up", "Down");
+
+func _physics_process(delta):
+	target.velocity = direction * speed
+	target.move_and_slide()
+#endregion
--- movement_bases/player_movement.gd
+++
@@ -1,8 +0,0 @@
-extends MovementScript
-#Authored by Ethan. Please consult for any modifications or major feature requests.
-
-#region Godot methods
-func _process(_delta):
-	#Set direction based on movement input
-	set_direction(Input.get_vector("Left", "Right", "Up", "Down"))
-#endregion
