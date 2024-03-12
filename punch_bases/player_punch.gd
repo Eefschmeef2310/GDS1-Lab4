b44273227f3ec0a58dad09dc6dcc5d2b0b6ed876
Pihoqub <111715046+Pihoqub@users.noreply.github.com>
2024-03-12 16:09:13 +1100

    Added Player Punches Script

---
+++ punch_bases/player_punch.gd
@@ -0,0 +1,39 @@
+extends PunchScript
+#class_name
+#Authored by [Name]. Please consult for any modifications or major feature requests.
+
+#region Variables
+#Signals
+
+#Enums
+
+#Constants
+
+#Exported Variables
+#@export_group("Group")
+#@export_subgroup("Subgroup")
+
+#Onready Variables
+
+#Other Variables (please try to separate and organise!)
+
+#endregion
+
+#region Godot methods
+func _ready():
+	#Runs when all children have entered the tree
+	pass
+
+func _process(delta):
+	input_punch = true if Input.is_action_pressed("Punch") else false
+	pass
+#endregion
+
+#region Signal methods
+
+#endregion
+
+#region Other methods (please try to separate and organise!)
+
+#endregion
+
--- punch_bases/punch_base.gd
+++ punch_bases/punch_base.gd
@@ -1,5 +1,5 @@
 extends Node
-#class_name
+class_name PunchScript
 #Authored by Alex. Please consult for any modifications or major feature requests.
 
 #region Variables
@@ -15,39 +15,38 @@ signal player_punched
 #@export_subgroup("Subgroup")
 
 #Onready Variables
-@onready var left_hitbox: CollisionShape2D = $"../../Body/LArm/LHand/LHitbox/CollisionShape2D"
-@onready var right_hitbox: CollisionShape2D = $"../../Body/RArm/RHand/RHitbox/CollisionShape2D"
+@onready var left_hitbox: CollisionShape2D = $"../../Body/LArm/LHand/LeftHitbox/CollisionShape2D"
+@onready var right_hitbox: CollisionShape2D = $"../../Body/RArm/RHand/RightHitbox/CollisionShape2D"
+
 #Other Variables (please try to separate and organise!)
 var can_punch: bool = true
 var can_retract: bool = false
 var is_right_arm: bool = true
 var current_arm_anim: AnimationPlayer
+
+var input_punch: bool = false #This variable determines the "input" from character
 #endregion
 
 #region Godot methods
-func _ready():
-	#Runs when all children have entered the tree
-	pass
-
-func _process(delta):
+func _physics_process(delta):
 	#Runs per frame
-	if can_punch && Input.is_action_pressed("Punch"):
+	if can_punch && input_punch:
 		punch(is_right_arm)
 	
-	if(can_retract && !Input.is_action_pressed("Punch")):
+	if(can_retract && !input_punch):
 		retract()
 		
 #endregion
 
 #region Signal methods
-func _on_left_hitbox_body_entered(body):
+func _on_punch_hitbox_body_entered(body):
+	#Functionality not added yet in other classes
+	#Should add score and knockback
 	if(body.is_in_group("Enemy")):
-		#Functionality not added yet in other classes
 		enemy_punched.emit()
 	if(body.is_in_group("Player")):
-		#Functionality not added yet in other classes
 		player_punched.emit()
-	pass # Replace with function body.
+
 #endregion
 
 #region Other methods (please try to separate and organise!)
@@ -70,13 +69,14 @@ func finished_l_punch():
 
 func retract():
 	current_arm_anim.play("retract")
-	right_hitbox.disabled = true
+	right_hitbox.disabled = true 
 	left_hitbox.disabled = true
 	can_retract = false
 	
 func finished_retract():
 	is_right_arm = !is_right_arm
 	can_punch = true
+
 #endregion
 
 
