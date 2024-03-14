41f5c11cdeca5628710f1691771fd8d37db2ac63
Pihoqub <111715046+Pihoqub@users.noreply.github.com>
2024-03-15 01:18:34 +1100

    Double punch added Extension Removed

--- punch_bases/punch_base.gd
+++ punch_bases/punch_base.gd
@@ -17,14 +17,19 @@ class_name PunchScript
 #Onready Variables
 @onready var left_hitbox: CollisionShape2D = $"../../Body/LArm/LHand/LeftHitbox/CollisionShape2D"
 @onready var right_hitbox: CollisionShape2D = $"../../Body/RArm/RHand/RightHitbox/CollisionShape2D"
-
+@onready var left_anim: AnimationPlayer = $"../../AnimationPlayers/LeftArm"
+@onready var right_anim: AnimationPlayer = $"../../AnimationPlayers/RightArm"
 #Other Variables (please try to separate and organise!)
-var can_punch: bool = true
-var can_retract: bool = false
-var is_right_arm: bool = true
-var current_arm_anim: AnimationPlayer
+var punch_buffer: float = 0.2
+var buffer_timer: float = 0
 
-var input_punch: bool = false #This variable determines the "input" from character
+var frequency_min: float = 0.3
+var frequency_timer: float = 0
+
+var can_r_punch: bool = true
+var can_l_punch: bool = true
+
+var is_right_arm: bool = true
 #endregion
 
 #region Godot methods
@@ -37,12 +42,11 @@ func _ready():
 
 func _physics_process(_delta):
 	#Runs per frame
-	if can_punch && input_punch:
+	if (can_l_punch || can_r_punch) && buffer_timer > 0 && frequency_timer < 0:
 		punch(is_right_arm)
 	
-	if(can_retract && !input_punch):
-		retract()
-		
+	buffer_timer -= _delta
+	frequency_timer -= _delta
 #endregion
 
 #region Signal methods
@@ -50,33 +54,41 @@ func _physics_process(_delta):
 #endregion
 
 #region Other methods (please try to separate and organise!)
+func input_punch():
+	buffer_timer = punch_buffer
+
+
 func punch(right: bool):
 	#play punch sound
 	if $"../../AudioPlayers/Punch" : $"../../AudioPlayers/Punch".play()
 	
-	current_arm_anim = $"../../AnimationPlayers/RightArm" if right else $"../../AnimationPlayers/LeftArm"
-	current_arm_anim.play("punch", -1, owner.punch_speed_scale)
+	buffer_timer = 0
+	frequency_timer = frequency_min
 	
-	left_hitbox.disabled = false
-	right_hitbox.disabled = false
+	if(right):
+		right_anim.play("punch", -1, owner.punch_speed_scale)
+		right_hitbox.disabled = false
+		can_r_punch = false
+	else:
+		left_anim.play("punch", -1, owner.punch_speed_scale)
+		left_hitbox.disabled = false
+		can_l_punch = false
+		
+	is_right_arm = !is_right_arm
 	
-	can_punch = false
-	
-func finished_punch():
-	can_retract = true
-	left_hitbox.disabled = true
-	right_hitbox.disabled = true
-
-func retract():
-	current_arm_anim.play("retract", -1, owner.retract_speed_scale)
+func retract_r():
+	right_anim.play("retract", -1, owner.retract_speed_scale)
 	right_hitbox.disabled = true 
-	left_hitbox.disabled = true
-	can_retract = false
-	
-func finished_retract():
-	is_right_arm = !is_right_arm #For now used to alternate between left and right punches
-	can_punch = true
 
+func retract_l():
+	left_anim.play("retract", -1, owner.retract_speed_scale)
+	left_hitbox.disabled = true 
+
+func finished_r_retract():
+	can_r_punch = true
+	
+func finished_l_retract():
+	can_l_punch = true
 #endregion
 
 
