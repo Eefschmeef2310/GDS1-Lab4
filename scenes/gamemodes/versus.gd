c7aac8b19853edba1a9680e051cef8c81040e907
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-03-15 00:14:11 +1100

    added funny fire shader

--- scenes/gamemodes/versus.gd
+++ scenes/gamemodes/versus.gd
@@ -25,25 +25,12 @@ func _ready():
 	battle.base_level.get_blue_player().set_as_player("p1_")
 	battle.base_level.get_red_player().set_as_player("p2_")
 
-func _process(delta):
+func _process(_delta):
 	#Runs per frame
 	pass
 #endregion
 
 #region Signal methods
-
-func _on_blue_player_hit(_knockback_power):
-	red_score += 1
-	score_changed.emit(blue_score, red_score)
-	check_game_end()
-
-func _on_red_player_hit(_knockback_power):
-	blue_score += 1
-	score_changed.emit(blue_score, red_score)
-	check_game_end()
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
 
 #endregion
 
