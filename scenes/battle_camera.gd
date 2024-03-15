ab317c869cd4417fb78044b10e9cc9ae7be94179
Cookie <51043100+cookiespl@users.noreply.github.com>
2024-03-15 16:38:13 +1100

    Cam shake disabled until fix is found

--- scenes/battle_camera.gd
+++ scenes/battle_camera.gd
@@ -1,55 +1,34 @@
 extends Camera2D
-#class_name
-#Authored by Xander. Please consult for any modifications or major feature requests.
 
-#region Variables
-#Signals
-
-#Enums
-
-#Constants
-
-#Exported Variables
-#@export_group("Group")
-#@export_subgroup("Subgroup")
-@export var decay := 1 #How quickly shaking will stop [0,1].
-@export var max_offset := Vector2(50,20) #Maximum displacement in pixels.
-@export var max_roll = 0.0 #Maximum rotation in radians (use sparingly).
+@export var decay := 0.8 #How quickly shaking will stop [0,1].
+@export var max_offset := Vector2(100,75) #Maximum displacement in pixels.
+@export var max_roll := 0.1 #Maximum rotation in radians (use sparingly).
 @export var noise : FastNoiseLite #The source of random values.
 
-#Onready Variables
-
-#Other Variables (please try to separate and organise!)
 var noise_y = 0 #Value used to move through the noise
 
 var trauma := 0.0 #Current shake strength
 var trauma_pwr := 3 #Trauma exponent. Use [2,3]
 
-#endregion
-
-#region Godot methods
 func _ready():
 	noise.seed = randi()
-
-func _process(delta):
-	if trauma:
-		trauma = max(trauma - decay * delta, 0)
-		shake()
-	#optional
-	elif offset.x != 0 or offset.y != 0 or rotation != 0:
-		lerp(offset.x,0.0,1)
-		lerp(offset.y,0.0,1)
-		lerp(rotation,0.0,1)
-#endregion
-
-#region Signal methods
-
-#endregion
-
-#region Other methods (please try to separate and organise!)
+	print(noise.seed)
 
 func add_trauma(amount : float):
+	zoom = Vector2(1.02, 1.02)
 	trauma = min(trauma + amount, 1.0)
+
+func _process(delta):
+	zoom = lerp(zoom, Vector2(1, 1), 3*delta)
+	
+	if trauma:
+		trauma = max(trauma - decay * delta, 0)
+		# shake()
+	#optional
+	elif offset.x != 0 or offset.y != 0 or rotation != 0:
+		offset.x = lerp(offset.x,0.0,1)
+		offset.y = lerp(offset.y,0.0,1)
+		rotation = lerp(rotation,0.0,1)
 
 func shake(): 
 	var amt = pow(trauma, trauma_pwr)
@@ -57,5 +36,6 @@ func shake():
 	rotation = max_roll * amt * noise.get_noise_2d(noise.seed,noise_y)
 	offset.x = max_offset.x * amt * noise.get_noise_2d(noise.seed*2,noise_y)
 	offset.y = max_offset.y * amt * noise.get_noise_2d(noise.seed*3,noise_y)
-
-#endregion
+	
+	print(snapped(noise.get_noise_2d(noise.seed*2,noise_y), 0.01))
+	
