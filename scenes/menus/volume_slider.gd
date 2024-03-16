ad94dcbc41bbecdf28da412bae38af0a5cfcbc4f
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-03-16 23:13:47 +1100

    added saved sound support

--- scenes/menus/volume_slider.gd
+++ scenes/menus/volume_slider.gd
@@ -15,10 +15,27 @@ func _ready():
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
 	
-	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+	match(bus_name):
+		"Master":
+			value = db_to_linear(SoundSaveManager.saved_settings.master)
+		"Music":
+			value = db_to_linear(SoundSaveManager.saved_settings.music)
+		"SFX":
+			value = db_to_linear(SoundSaveManager.saved_settings.sfx)
+	
+	#value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
 #endregion
 
 #region Signal methods
 func _on_value_changed(_value):
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+	
+	#store to sound settings
+	match(bus_name):
+		"Master":
+			SoundSaveManager.saved_settings.master = AudioServer.get_bus_volume_db(bus_index)
+		"Music":
+			SoundSaveManager.saved_settings.music = AudioServer.get_bus_volume_db(bus_index)
+		"SFX":
+			SoundSaveManager.saved_settings.sfx = AudioServer.get_bus_volume_db(bus_index)
 #endregion
---
+++ sound_save_system/sound_save_manager.gd
@@ -0,0 +1,27 @@
+extends Node
+#Authored by Ethan. Please consult for any modifications or major feature requests.
+
+#region Variables
+	#Constants
+const save_path : String = "user://soundsettings.tres"
+
+	#Other Variables (please try to separate and organise!)
+var saved_settings : SoundSettings
+#endregion
+
+#region Godot methods
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	if FileAccess.file_exists(save_path):
+		load_save()
+	else:
+		saved_settings = SoundSettings.new()
+#endregion
+
+#region Other methods (please try to separate and organise!)
+func save():
+	ResourceSaver.save(saved_settings, save_path)
+	
+func load_save():
+	saved_settings = ResourceLoader.load(save_path)
+#endregion
